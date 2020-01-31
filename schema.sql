a89c141984cbbfbe7896d2be9853d7625fb6f2a8
Abdullahmed98 <56558841+Abdullahmed98@users.noreply.github.com>
2020-01-31 23:54:48 +0000

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -3,27 +3,31 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT,
+    dep_id INT NOT NULL AUTO_INCREMENT,
     dep_name varchar(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (dep_id)
 );
 
 CREATE TABLE employees (
-    Emp_id INT NOT NULL AUTO_INCREMENT,
+    emp_id INT NOT NULL AUTO_INCREMENT,
+    role_id INT,
     first_name varchar(30),
     last_name varchar(30),
-    PRIMARY KEY(Emp_id)
+    PRIMARY KEY (emp_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT,
+    dep_id INT,
     title varchar(30),
     salary decimal,
-    PRIMARY KEY(role_id)
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dep_id) REFERENCES departments(dep_id)
 );
 
 INSERT INTO departments VALUES(1,'Production'),(2,"Research"),(3,"Marketing"),(4,"Human Resource"),(5,"Finane");
 
-INSERT INTO roles VALUES(1, "Production Lead", 100000),(2, "Production Junior", 50000),(3,"Research Lead",80000),(4,"Research Junior",40000),(5,"Marketing Lead", 110000),(6,"Marketing Junior", 65000),(7,"Human Resource Lead", 75000),(8,"Human Resource Junior",37750),(9,"Finance Lead", 90000),(10,"Finance Junior",45000);
+INSERT INTO roles VALUES(1,1, "Production Lead", 100000),(2,2, "Production Junior", 50000),(3,3,"Research Lead",80000),(4,4,"Research Junior",40000),(5,5,"Marketing Lead", 110000),(6,6,"Marketing Junior", 65000),(7,7,"Human Resource Lead", 75000),(8,8,"Human Resource Junior",37750),(9,9,"Finance Lead", 90000),(10,10,"Finance Junior",45000);
 
-INSERT INTO employees VALUES(1,"Carol","Hammond"),(2,"Antoinette", "Tate"),(3,"Priscilla","Moran"),(4,"Seth","Weber"),(5,"Ida","Reeves"),(6,"Kent","Warren"),(7,"Tracy","Black"),(8,"Keith","Warren"),(9,"William","James"),(10,"Al","Rose"),(11,"Frances","Lopez"),(12,"Joyce", "Wilson"),(13,"Norma","Morgan"),(14,"Jean","Brooks"),(15,"Sarah","Johnson"),(16,"Alice","Stewart"),(17,"Alan Lewis"),(18,"Pamela","Reed"),(19,"Craig","Wright"),(20,"Larry","Howard");
+INSERT INTO employees VALUES(1,1,"Carol","Hammond"),(2,2,"Antoinette", "Tate"),(3,3,"Priscilla","Moran"),(4,4,"Seth","Weber"),(5,5,"Ida","Reeves"),(6,6,"Kent","Warren"),(7,7,"Tracy","Black"),(8,8,"Keith","Warren"),(9,9,"William","James"),(10,10,"Al","Rose");
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,8 +8,21 @@ var connection = mysql.createConnection({
     password: "root",
     database: "employee_tracker_db"
 });
-connection.connect();
 
-var display = function() {
-    connection.query("SELECT * FROM products")
-};
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    queryshowtable();
+});
+
+function queryshowtable() {
+    connection.query("SELECT employees.emp_id, employees.first_name, employees.last_name, roles.title, departments.dep_name, roles.salary FROM employees, roles, departments", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+           console.log([res[i].employees.emp_id, res[i].employees.first_name, res[i].employees.last_name, res[i].roles.title, res[i].departments.dep_name, res[i].roles.salary]) 
+        }
+    });
+
+}
+
+
+
